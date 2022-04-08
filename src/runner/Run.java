abb5e57dc00df2aa81ce5d787600bd795da32a8f
Jorge E M Huertas <jorge.martinez09@uptc.edu.co>
2022-04-08 16:21:50 -0500

    Change menu a other branch

--- src/runner/Run.java
+++ src/runner/Run.java
@@ -2,9 +2,12 @@ package runner;
 
 import presenter.Presenter;
 
+import javax.swing.*;
+
 public class Run {
     public static void main(String[] args) {
         new Presenter();
         System.out.println("First commit");
+        JOptionPane.showMessageDialog(null, "Menu");
     }
 }
